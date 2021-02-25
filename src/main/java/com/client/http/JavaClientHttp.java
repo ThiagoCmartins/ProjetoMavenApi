a4dacdf621289ef3b67ec6ba62219449985024de
Thiago Correia <nathimartins_@uni9.edu.br>
2021-02-25 14:07:01 -0300

    println

--- src/main/java/com/client/http/JavaClientHttp.java
+++ src/main/java/com/client/http/JavaClientHttp.java
@@ -8,20 +8,27 @@ import java.net.http.HttpResponse;
 
 public class JavaClientHttp {
 	
-	String cep = "01001000";
-	String api = "https://viacep.com.br/ws/"+cep+"/json/";
-	
-	private static final String APP_URL_CEP = "https://viacep.com.br/ws/01001000/json/";;
+	private static final String APP_URL_CEP = "https://viacep.com.br/ws/01001000/json/";
 	
 	public static void main(String[] args) throws IOException, InterruptedException {
 		
 		HttpClient client = HttpClient.newHttpClient();
+		
+		System.out.println("client: "+client);
+		System.out.println("-------------------------");
+		
 		HttpRequest request = HttpRequest.newBuilder()
 				.GET()
 				.uri(URI.create(APP_URL_CEP))
-				.build(); 
+				.build();
+		
+		System.out.println("request: "+request);
+		System.out.println("-------------------------");
+		
 		HttpResponse<String> response = client.send(request, HttpResponse.BodyHandlers.ofString());
 		
-		System.out.println(response);
+		System.out.println("response: "+ response);
+		System.out.println("-------------------------");
+		System.out.println("response body:"+response.body());
 	}
 }
