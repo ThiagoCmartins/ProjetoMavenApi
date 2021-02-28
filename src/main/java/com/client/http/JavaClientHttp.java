1b40433969453f5fa986b80ec722b531f23d8d18
Thiago Correia <nathimartins_@uni9.edu.br>
2021-02-27 21:38:14 -0300

    JavaClienteHttpOK

--- src/main/java/com/client/http/JavaClientHttp.java
+++ src/main/java/com/client/http/JavaClientHttp.java
@@ -1,41 +1,59 @@
 package com.client.http;
 
-import java.io.IOException;
-
 import java.net.URI;
 import java.net.http.HttpClient;
 import java.net.http.HttpRequest;
 import java.net.http.HttpResponse;
+
 import com.get.set.Endereco;
+import com.google.gson.Gson;
 
 public class JavaClientHttp {
 	
-	private static final String APP_URL_CEP = "https://viacep.com.br/ws/01001000/json/";
+	public static void main(String[] args) throws Exception {newEndereco();}
 	
-	public static void main(String[] args) throws IOException, InterruptedException {
+	static String API_URL = "https://viacep.com.br/ws/01001000/json/";
+    static int ok = 200;
+	
+	public static Endereco newEndereco()throws Exception{
 		
-		HttpClient client = HttpClient.newHttpClient();
+		try {
 		
-		System.out.println("client: "+client);
-		System.out.println("-------------------------");
+			HttpClient client = HttpClient.newHttpClient();
+			
+			HttpRequest request = HttpRequest.newBuilder()
+					.GET()
+					.uri(URI.create(API_URL))
+					.build();
+			
+			HttpResponse<String> response = client.send(request, HttpResponse.BodyHandlers.ofString());
+			
+			if (response.statusCode() != ok) {
+				throw new RuntimeException("HTTP error code : " + response.statusCode());
+			}
+			
+			Gson gson = new Gson();
+	        Endereco enderecos = gson.fromJson(response.body(), Endereco.class);
+			
+			System.out.println("client: "+client);
+			System.out.println("-------------------------");
+			
+			System.out.println("request: "+request);
+			System.out.println("-------------------------");
+			
+			System.out.println("Conexão Status: "+response.statusCode());
+			
+			System.out.println("response: "+ response);
+			System.out.println("-------------------------");
+			System.out.println("response body:"+response.body());
+	        
+	        System.out.println("-------------------------");
+			System.out.println("enderecos: "+enderecos);
 		
-		HttpRequest request = HttpRequest.newBuilder()
-				.GET()
-				.uri(URI.create(APP_URL_CEP))
-				.build();
+		return enderecos;
 		
-		System.out.println("request: "+request);
-		System.out.println("-------------------------");
-		
-		HttpResponse<String> response = client.send(request, HttpResponse.BodyHandlers.ofString());
-		
-		//response.statusCode();
-		System.out.println("Conexão: "+response.statusCode());
-		
-		System.out.println("response: "+ response);
-		System.out.println("-------------------------");
-		System.out.println("response body:"+response.body());
-		
-		
+		}catch (Exception e) {
+	        throw new Exception("ERRO: " + e);
+	    }
 	}
 }
--- src/main/java/com/controller/controller.java
+++ src/main/java/com/controller/controller.java
@@ -1,9 +1,41 @@
 package com.controller;
 
-public class controller {
+import java.io.IOException;
 
-	public controller() {
-		
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import com.client.http.JavaClientHttp;
+
+public class controller extends HttpServlet {
+	
+	private static final long serialVersionUID = 1L;
+       
+    public controller() {
+        super();
+    }
+
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		response.getWriter().append("Served at: ").append(request.getContextPath());
 	}
 
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		doGet(request, response);
+		response.setContentType("text/html;charset=UTF-8");
+        request.setCharacterEncoding("UTF-8");
+        response.setCharacterEncoding("UTF-8");
+        
+        try {
+        	
+        	 JavaClientHttp.newEndereco();
+        	
+	        request.setAttribute("listaEndereco", JavaClientHttp.newEndereco());
+	        request.getRequestDispatcher("inicial.jsp").forward(request, response);
+        
+        }catch (Exception e) {
+            new Exception("ERRO: " + e);
+        }
+	}
 }
